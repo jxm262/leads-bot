c1950d5eb789ecc24e5ba7838c2812957b0a2ad6
Nick Christenson <emailfornc@gmail.com>
2017-09-19 19:56:23 -0400

    pulled the message sender into its own class

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import fs from 'fs';
-import request from 'superagent';
 import dataImporter from './data-importer';
+import {sendMessageToSlack} from './message-sender.js'
 
 console.log(dataImporter);
 
-
-function sendMessageToSlack(msg, webHookUrl="https://hooks.slack.com/services/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") {
-  request
-   .post(webHookUrl)
-   .send(msg)
-   .set('Accept', 'application/json')
-   .end(function(err, res){
-    
-   });
-}    
-
- sendMessageToSlack({ text: 'another one bites the dust' });
+sendMessageToSlack({ text: 'boom' });
