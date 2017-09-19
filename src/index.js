d2c07bddb4bba66c1b8ce656cf635d5b2685e30a
justin maat <jxm262@gmail.com>
2017-09-19 19:34:54 -0400

    more shuffling of dirs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-import fs from 'fs';
-import request from 'superagent';
+import dataImporter from './data-importer';
 
-
-request
-    .get('https://www.reddit.com/r/rubyjobs/new.json')
-    .set('USER-AGENT', 'node:devguild-bot:v1 (by /u/jxm262)')
-    .then((resp) => {
-        console.log('found data...');
-        console.log('body.. ', resp.body.data.children[1]);
-    })
-    .catch((err) => {
-        console.log('error somewhere ', err);
-    });
-
-
+console.log(dataImporter);
