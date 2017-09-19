7abbf4fecfaa5c83c5fb6bd89ce700bcad8dde70
justin maat <jxm262@gmail.com>
2017-09-19 19:23:50 -0400

    moving splitting up src and test dirs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import request from 'superagent';
 
 
@@ -5,17 +6,11 @@ request
     .get('https://www.reddit.com/r/rubyjobs/new.json')
     .set('USER-AGENT', 'node:devguild-bot:v1 (by /u/jxm262)')
     .then((resp) => {
-        // response should look something like -
-        // {
-        //     "access_token": "J1qK1c18UUGJFAzz9xnH56584l4",
-        //     "expires_in": 3600,
-        //     "scope": "*",
-        //     "token_type": "bearer"
-        // }
-
         console.log('found data...');
-        console.log('body.. ', resp.body);
+        console.log('body.. ', resp.body.data.children[1]);
     })
     .catch((err) => {
         console.log('error somewhere ', err);
     });
+
+
