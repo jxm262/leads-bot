def964df804ca0112f42da73407fa86a059e45fd
Nick Christenson <emailfornc@gmail.com>
2017-09-19 19:39:36 -0400

    added a function to post to slack

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import fs from 'fs';
 import request from 'superagent';
 
 
-request
-    .get('https://www.reddit.com/r/rubyjobs/new.json')
-    .set('USER-AGENT', 'node:devguild-bot:v1 (by /u/jxm262)')
-    .then((resp) => {
-        console.log('found data...');
-        console.log('body.. ', resp.body.data.children[1]);
-    })
-    .catch((err) => {
-        console.log('error somewhere ', err);
-    });
+// request
+//     .get('https://www.reddit.com/r/rubyjobs/new.json')
+//     .set('USER-AGENT', 'node:devguild-bot:v1 (by /u/jxm262)')
+//     .then((resp) => {
+//         console.log('found data...');
+//         console.log('body.. ', resp.body.data.children[1]);
+//     })
+//     .catch((err) => {
+//         console.log('error somewhere ', err);
+//     });
 
 
+
+function sendMessageToSlack(msg, webHookUrl="https://hooks.slack.com/services/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") {
+  request
+   .post(webHookUrl)
+   .send({ text: msg })
+   .set('Accept', 'application/json')
+   .end(function(err, res){
+    
+   });
+}    
+
+ sendMessageToSlack('testing');
