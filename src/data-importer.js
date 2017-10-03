110512c0889744a59cbacc47942496937e93c079
justin maat <jxm262@gmail.com>
2017-10-03 14:47:52 -0400

    fixing all issues on filtering and retrieving

--- src/data-importer.js
+++ src/data-importer.js
@@ -1,9 +1,16 @@
-import fs from 'fs';
 import request from 'superagent';
 import _ from 'lodash';
+import fs from 'fs';
+
+// fuck it i'm lazy
+const AWS = require('aws-sdk');
 
 
-const leadsFile = __dirname + '/../leads.json';
+const s3 = new AWS.S3();
+const params = {
+  Bucket : 'devguild',
+  Key : 'leads.json'
+};
 
 export function removeUnecessaryProps(p) {
   return (({ id, selftext, title, permalink }) => ({ id, selftext, title, permalink }))(p);
@@ -11,34 +18,43 @@ export function removeUnecessaryProps(p) {
 
 export function filterLeads(leads) {
 
-  const oldLeadIds = JSON
-    .parse(fs.readFileSync(leadsFile, 'utf8'))
-    .map(p => p.id);
+  return s3.getObject(params).promise()
+    .then(d => {
 
-  const newLeads = leads
-    .filter(p => !_.includes(oldLeadIds, p.id))
-    .map(p => removeUnecessaryProps(p));
+      const oldLeads = JSON.parse(d.Body.toString('utf-8'));
+      const oldLeadIds = oldLeads.map(p => p.id);
 
-  // return last 200 for now
-  const truncated = newLeads.slice(0, 200);
+      console.log('---ids', oldLeadIds);
+      console.log('--newlead ids ', leads.map(p => p.id));
 
-  fs.writeFileSync(leadsFile, JSON.stringify(truncated), 'utf8');
+      const newLeads = leads
+        .filter(p => {
+          console.log('p.id ', p.id);
+          return !_.includes(oldLeadIds, p.id);
+        })
+        .map(p => removeUnecessaryProps(p));
 
-  return truncated;
+
+      console.log('---new Leads filtered ', newLeads.map(p => p.id));
+
+
+      // api returns 25
+      const truncated = newLeads.concat(oldLeads.slice(0, 200));
+
+      return s3.putObject(Object.assign({}, params, {Body: JSON.stringify(truncated), ContentType: 'application/json'})).promise()
+        .then(p => {
+          return Promise.resolve(newLeads);
+        });
+    })
 }
 
 export function importLeads() {
   return request
-    .get('https://www.reddit.com/r/rubyjobs/new.json')
+    .get('https://www.reddit.com/r/railsjobs/new.json')
     .set('USER-AGENT', 'node:devguild-bot:v1 (by /u/jxm262)')
     .then((resp) => {
       const leads = resp.body.data.children.map(p => p.data);
-
-      console.log('1st entry for troubleshooting purposes - ', leads[0]);
-
-      const filteredLeads = filterLeads(leads);
-
-      return Promise.resolve(filteredLeads);
+      return filterLeads(leads);
     })
     .catch((err) => {
       console.log('error somewhere ', err);
--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import { importLeads } from './data-importer';
 import { sendMessageToSlack } from './message-sender.js'
 
 
-function sendLeads(leads) {
-  leads.slice(0, 5).forEach(lead => {
-    const msg = `Title: <https://www.reddit.com${lead.permalink}|${lead.title}>`;
-    const description = `${lead.selftext || '- no description -'}`;
-    sendMessageToSlack({ text: msg, attachments: [{ text: description }] });
-  });
-  console.log('done..');
-};
+exports.handler = function (event, context, callback) {
 
-importLeads()
-  .then(p => sendLeads(p));
+  function sendLeads(leads) {
+    console.log('---new ledas ', leads.length);
+    leads.forEach(lead => {
+      const msg = `Title: <https://www.reddit.com${lead.permalink}|${lead.title}>`;
+      const description = `${lead.selftext || '- no description -'}`;
+      sendMessageToSlack({ text: msg, attachments: [{ text: description }] });
+    });
+  };
+
+  importLeads()
+    .then(p => sendLeads(p))
+    .catch(e => {
+      console.log('some err ', e);
+      callback(e);
+    });
+
+};
